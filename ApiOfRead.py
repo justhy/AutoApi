1e8553b4f3f4c7050a53b0822c1314895679febd
justhy <hy4396@gmail.com>
2021-03-03 01:56:56 +0800

    Update ApiOfRead.py

--- ApiOfRead.py
+++ ApiOfRead.py
@@ -33,9 +33,9 @@ log_list=[0]*app_count
 config = {
          'api_rand': 1,
          'rounds': 3,
-         'rounds_delay': [1,600,1200],
-         'api_delay': [1,600,2000],
-         'app_delay': [1,300,600],
+         'rounds_delay': [1,60,600],
+         'api_delay': [1,60,600],
+         'app_delay': [1,60,600],
          }
 api_list = [
            r'https://graph.microsoft.com/v1.0/me/',
